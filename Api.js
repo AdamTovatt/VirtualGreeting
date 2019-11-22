61eaa69b975d76617ef420f076d45eb532764e7f
Adam Tovatt <ato@citygate.se>
2019-11-22 16:00:02 +0100

    fix

---
+++ Api.js
@@ -0,0 +1,44 @@
+class Api {
+    constructor() {
+        this.basePath = "http://virtual-greeting.herokuapp.com/";
+    }
+
+    async GetResponse(method, url, data = null) {
+        var requestUrl = this.basePath + url;
+        return new Promise(function (resolve, reject) {
+            let xhr = new XMLHttpRequest();
+            xhr.open(method, requestUrl);
+            xhr.onload = function () {
+                resolve(xhr.response);
+            };
+            xhr.onerror = function () {
+                reject({
+                    status: this.status,
+                    statusText: xhr.statusText
+                });
+            };
+            if (data != null) {
+                xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+                xhr.send(JSON.stringify(data));
+            }
+            else {
+                xhr.send();
+            }       
+        });
+    }
+
+    async Init() {
+        var path = "greeting/init";
+        return await this.GetResponse("POST", path);
+    }
+
+    async GetGreetingData(id, hash) {
+        var path = "greeting/view/?id=" + id + "&hash=" + hash;
+        return await this.GetResponse("GET", path);
+    }
+
+    async UpdateGreetingData(id, hash, data) {
+        var path = "greeting/update/?id=" + id + "&hash=" + hash;
+        return await this.GetResponse("POST", path, data);
+    }
+}

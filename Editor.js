2d19a47379c52851ced9812f6819f158ff723571
Adam Tovatt <ato@citygate.se>
2019-11-25 11:43:10 +0100

    fix

--- Editor.js
+++ Editor.js
@@ -1,19 +1,25 @@
 class Editor {
-    constructor(id, editHash, api, viewHash = null) {
+    constructor(api, id, editHash, title = null, viewHash = null, created = null) {
         this.id = id;
         this.editHash = editHash;
         this.viewHash = viewHash;
+        this.title = title;
+        this.created = created;
         this.api = api;
+        if (created == null)
+            created = Date.now();
     }
 
     async initialize() {
         console.log("getting hash");
-        console.log(this.id);
-        console.log(await api.GetGreetingData(this.id, this.editHash));
+        var data = JSON.parse(await api.GetGreetingData(this.id, this.editHash));
+        this.viewHash = data["hash"];
+        this.created = data["created"];
+        this.title = data["title"];
     }
 
-    static async create(id, editHash, api, viewHash = null) {
-        const o = new Editor(id, editHash, api, viewHash = null);
+    static async create(api, id, editHash, title, viewHash = null, created = null) {
+        var o = new Editor(api, id, editHash, title, viewHash = null, created);
         if (viewHash == null) {
             await o.initialize();
         }
